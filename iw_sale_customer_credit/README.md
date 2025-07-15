e381aac06d414ea8b0803c2fcd0c2bd173fef0da
Ismail Khan <73880897+ismailwaxer@users.noreply.github.com>
2025-07-16 02:57:32 +0300

    iw sale customer credit limit

---
+++ iw_sale_customer_credit/README.md
@@ -0,0 +1,24 @@
+# Sale Customer Credit Limit
+
+Displays the customer's credit limit and related financial information directly on the sale order form.
+
+## Key Features
+
+- Show customer credit limit on Sale Orders
+- Displays available credit and Days Sales Outstanding (DSO)
+- Provides instant credit visibility for the sales team
+
+## Screenshots
+
+![Main Screenshot](static/description/main_screenshot.png)
+
+## Installation
+
+1. Add the module to your Odoo addons folder
+2. Update the app list and install via Odoo Apps
+
+## Author
+
+Ismail Waxir
+
+Contact: ismailwaxir@gmail.com
---
+++ iw_sale_customer_credit/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "IW Sale Customer Credit Limit",
+    'summary': "Displays the customer's credit limit and related information directly on the sale order form.",
+    'description': """
+This module extends the Sale Order functionality by displaying the customer's credit limit, available credit, and Days Sales Outstanding (DSO) on the sale order form. 
+These values are automatically fetched from the selected customer (partner) and kept up-to-date, allowing sales teams to quickly assess a customer's credit status before confirming sales.
+
+Key Features:
+- Shows customer credit limit on the sale order.
+- Displays available credit and total receivables.
+- Presents Days Sales Outstanding (DSO) for the selected customer.
+- Enhances sales and credit control visibility.
+
+Ideal for businesses that need to monitor customer credit directly from the sales process.
+    """,
+
+    'author': "Ismail Waxir",
+    'website': "https://github.com/ismailwaxer/v18.git",
+    'category': 'Sales',
+    'version': '18.0.1.0.0',
+
+    'license': 'LGPL-3',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale', 'stock'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+
+    'images': [
+        'static/description/icon.png',  # Logo/Icon
+        'static/description/main_screenshot.png',  # First Feature/Screenshot
+    ],
+
+    'application': False,
+    'installable': True,
+}
